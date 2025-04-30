917bf4afd97b5f1ef61beb24a6bb6b925753ab75
Anuja Sharma <120904105+AnujaSharma02@users.noreply.github.com>
2025-04-30 20:05:54 +0530

    Create DBConnection.java

---
+++ backend/DBConnection.java
@@ -0,0 +1,6 @@
+public class DBConnection {
+    public static void connect() {
+        // Add actual JDBC connection logic here
+        System.out.println("Connected to MySQL database!");
+    }
+}
